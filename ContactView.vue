7fcc46045435a741e0983c7fa37bbe48fbeafb56
ericawjc <142936336+ericawjc@users.noreply.github.com>
2023-10-11 13:34:45 +1100

    Add files via upload

---
+++ ContactView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="contact">
+    <h1>Contact Me</h1>
+  </div>
+  <p>Phone: 0481399190</p>
+  <p>Email: ericawj.chen6@gmail.com</p>
+  <form action="/action_page.php">
+<fieldset>
+  <legend>Inquiries:</legend>
+  <label for="Name">Name:</label><br>
+  <input type="text" id="Name" name="Name" value="John Doe"><br>
+  <label for="Email">Email:</label><br>
+  <input type="text" id="Email" name="Email" value="john.doe@gmail.com"><br><br>
+  <form action="/action_page.php">
+<br><br>
+<p>Your message is: {{ message }}</p>
+	<input v-model="message" placeholder="Type your message here" />
+<input type="submit">
+</form>
+</fieldset>
+</form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('')
+</script>
+
+<style scoped>
+
+</style>
---
+++ HomeView.vue
@@ -0,0 +1,77 @@
+<template>
+  <head>
+<title>Bootstrap 5 Example</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+  <body>
+
+  <div class="container-fluid p-5 bg-primary text-white text-center">
+      <h1>Erica Chen's SIT120 Portfolio</h1>
+      <p>Quality-driven games designer with excellent verbal, critical thinking and collaboration skills. Highly adaptable with a special love for creative writing and storytelling. Experience working with Unreal Engine and Blender. Adept at successfully managing projects from concept through to completion as well as crafting rich, compelling user experiences.</p> 
+  </div>
+
+  <h2>Experience</h2>
+
+  <table style="width:100%">
+<tr>
+  <th>Company</th>
+  <th>Position</th>
+  <th>Role</th>
+</tr>
+<tr>
+  <td>SMG Studio, 2018 </td>
+  <td>Internship - Fast & Furious Takedown </td>
+  <td>Analyzed and tested game builds to determine bugs and provide assistance in solving issues. Entered and sorted data accurately after establishing data entry priorities and procedures. </td>
+</tr>
+<tr>
+  <td>Academy of Interactive Entertainment, 2018</td>
+  <td>Student Project - Who's A Good Boy? </td>
+  <td>Collaborated with artists, programmers and sound engineers to produce a playable prototype within a 6 week deadline. Managed all design and production tasks from concept through to completion. Developed and maintained all documentation, pipelines and recorded weekly logs of iteration and progress. Implemented game assets into Unity and tested builds on a daily basis.</td>
+</tr>
+<tr>
+  <td>Plastic Wax, 2015</td>
+  <td>Work Placement - Warhammer 40,000: Regicide </td>
+  <td>Analyzed and tested game builds to determine bugs and provide assistance in solving issues. Utilized Autodesk 3ds Max to create realistic characters with full range of emotions and movement. Combined technical and artistic abilities to accomplish challenging animation objectives.</td>
+</tr>
+<tr>
+  <td>Supervixen, 2014</td>
+  <td>Work Experience</td>
+  <td>Collaborated with various teammates when working on day-to-day tasks and devising new concepts. Brainstormed different ideas and images for a new gaming concept. Took ideas and developed high quality creations, which included characters, environments and other objects.</td>
+</tr>
+</table>
+
+  <h2>Skills</h2>
+  <p>Concept design and documentation</p>
+  <p>Project management</p>
+  <p>Written and verbal communication</p>
+  <p>Prototyping</p>
+  <p>Unreal Engine and Unity</p>
+  <p>Blender and Maya</p>
+
+  </body>
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+table, th, td {
+border:1px solid black;
+}
+body {
+background-color: lightgreen;
+}
+
+@media only screen and (orientation: landscape) {
+body {
+  background-color: lightblue;
+}
+}
+</style>
